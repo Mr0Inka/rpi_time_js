56bfd0c4e032970cf68518df357d9d6cedc01daf
Thomas Weiß <thomas0weiss@googlemail.com>
2019-04-01 10:02:46 +0200

    Create time.js

---
+++ time.js
@@ -0,0 +1,19 @@
+let request = require("request");
+let timeJob = require('child_process').exec;
+
+apiTime();
+
+function apiTime(){
+    request("http://worldtimeapi.org/api/ip", function(error, response, body){
+        if(error == null){
+	    let timer = JSON.parse(body);
+            console.log(" > Time set successfully");
+            timeJob('sudo date -s "' + timer.datetime + '"; echo "hello"');
+	} else {
+	    console.log(" > Error loading time: " + error);
+	    setTimeout(function(){
+		apiTime();
+	    }, 3000);
+	}
+    });
+}
